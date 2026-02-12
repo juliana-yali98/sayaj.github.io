fe4c028e0c0148cee8f62e308f8b932cf7a20bcb
juliana-yali98 <jsanchezy@unal.edu.co>
2026-02-11 22:44:21 -0500

    rr

--- src/components/Redes.js
+++ src/components/Redes.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect, useRef } from 'react';
 import { MapContainer, TileLayer, Polyline, CircleMarker, Popup, Tooltip } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
-import './RedesConMapa.css';
+import './Redes.css';
 
 const RedesConMapa = () => {
   const [activeTab, setActiveTab] = useState('antioquia');
